089755a2c9e2883b5ccbd7e5058e63aa71dfe859
LuigiThiago <159830259+LuigiThiago@users.noreply.github.com>
2024-02-12 18:34:55 -0400

    Perguntas sobre Taylor Swift

--- index.js
+++ index.js
@@ -1,95 +1,96 @@
 const perguntas = [
   {
-    pergunta: "Qual é o nome completo do personagem principal?",
+    pergunta: "Qual é o nome completo de Taylor Swift?",
     respostas: [
-      "A) Harry James Potter",
-      "B) Harry Sirius Potter",
-      "C) Harry Severus Potter",
-    ],
-    correta: 0
-  },
-  {
-    pergunta: "Qual é a casa de Hogwarts à qual Harry Potter pertence?",
-    respostas: [
-      "A) Grifinória",
-      "B) Sonserina",
-      "C) Corvinal",
-    ],
-    correta: 0
-  },
-  {
-    pergunta: "Quem é o melhor amigo de Harry Potter?",
-    respostas: [
-      "A) Hermione Granger",
-      "B) Ron Weasley",
-      "C) Draco Malfoy",
+      "A) Taylor Marie Swift",
+      "B) Taylor Alison Swift",
+      "C) Taylor Elizabeth Swift",
     ],
     correta: 1
   },
   {
-    pergunta: "Qual é a forma do Patrono de Harry Potter?",
+    pergunta: "Qual foi o álbum de estreia de Taylor Swift?",
     respostas: [
-      "A) Leão",
-      "B) Veado",
-      "C) Águia",
+      "A) Speak Now",
+      "B) Fearless",
+      "C) Taylor Swift",
+    ],
+    correta: 2
+  },
+  {
+    pergunta: "Qual é o nome do primeiro single de Taylor Swift?",
+    respostas: [
+      "A) Love Story",
+      "B) Tim McGraw",
+      "C) Our Song",
     ],
     correta: 1
   },
   {
-    pergunta: "Quem matou os pais de Harry Potter?",
+    pergunta: "Quantos prêmios Grammy Taylor Swift já ganhou até 2022?",
     respostas: [
-      "A) Voldemort",
-      "B) Snape",
-      "C) Bellatrix Lestrange",
-    ],
-    correta: 0
-  },
-  {
-    pergunta: "Qual é o nome do elfo doméstico que serve à família Malfoy?",
-    respostas: [
-      "A) Dobby",
-      "B) Kreacher",
-      "C) Winky",
+      "A) 8",
+      "B) 10",
+      "C) 11",
     ],
     correta: 2
   },
   {
-    pergunta: "Qual é a posição de quadribol de Harry Potter?",
+    pergunta: "Em que ano Taylor Swift nasceu?",
     respostas: [
-      "A) Apanhador",
-      "B) Batedor",
-      "C) Goleiro",
+      "A) 1987",
+      "B) 1989",
+      "C) 1990",
     ],
     correta: 0
   },
   {
-    pergunta: "Qual é o feitiço que faz objetos levitarem?",
+    pergunta: "Qual é o nome do álbum de Taylor Swift lançado em 2020?",
     respostas: [
-      "A) Lumus",
-      "B) Expecto Patronum",
-      "C) Wingardium Leviosa",
+      "A) Reputation",
+      "B) Lover",
+      "C) Folklore",
     ],
     correta: 2
   },
   {
-    pergunta: "Qual é o nome da escola frequentada por Harry Potter?",
+    pergunta: "Qual é o nome do irmão de Taylor Swift?",
     respostas: [
-      "A) Escola de Bruxaria de Hogwarts",
-      "B) Escola de Magia de Beauxbatons",
-      "C) Escola de Magia e Feitiçaria de Durmstrang",
+      "A) Austin Swift",
+      "B) Scott Swift",
+      "C) Jake Swift",
     ],
     correta: 0
   },
   {
-    pergunta: "Qual é o animal de estimação de Hermione Granger?",
+    pergunta: "Qual é o signo do zodíaco de Taylor Swift?",
     respostas: [
-      "A) Gato",
-      "B) Coruja",
-      "C) Sapo",
+      "A) Virgem",
+      "B) Sagitário",
+      "C) Áries",
     ],
     correta: 0
+  },
+  {
+    pergunta: "Qual é o nome do documentário sobre Taylor Swift lançado em 2020?",
+    respostas: [
+      "A) Miss Americana",
+      "B) The Swift Life",
+      "C) Speak Now",
+    ],
+    correta: 0
+  },
+  {
+    pergunta: "Qual é a altura aproximada de Taylor Swift?",
+    respostas: [
+      "A) 1,63 m",
+      "B) 1,70 m",
+      "C) 1,78 m",
+    ],
+    correta: 1
   },
 ];
+
   
   const quiz = document.querySelector('#quiz')
   const template = document.querySelector('template')
